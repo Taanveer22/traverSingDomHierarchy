24c412513f954e3af85780f06fa1bbca05e0882c
Taanveer22 <taanveer469@gmail.com>
2025-05-05 15:05:21 +0600

    alhamdulliah traversing dom hierarchy file organized via topics

--- script2.js
+++
@@ -1,9 +0,0 @@
-console.log('connected 2');
-
-const para = document.querySelector('.para-container');
-console.log(para);
-// innerText doesnot show the hidden value
-console.log(para.innerText);
-
-// but textContext show the hidden value also
-console.log(para.textContent);
